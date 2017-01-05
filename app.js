d5cc4f9b5bc25608024d78ee5ff7908df955a8b6
David Mihal <david@dmihal.com>
2017-01-06 00:04:35 +0200

    Support a post request to root

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var app = express();
 
 var oneDay = 86400000;
 
+app.post("/", function(req, res, next) {
+  req.method = "GET";
+  next();
+});
 app.use(express.static(__dirname + '/static', { maxAge: oneDay }));
 app.use('/api', api);
 
